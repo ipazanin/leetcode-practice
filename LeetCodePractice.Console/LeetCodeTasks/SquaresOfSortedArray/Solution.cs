4280fe5a28fc5d44d9bebabd8ccc45629939116b
Ivan Pažanin <ivan.pazanin1996@gmail.com>
2024-03-02 16:27:10 +0100

    Add Squares Of Sorted Array Solution

---
+++ LeetCodePractice.Console/LeetCodeTasks/SquaresOfSortedArray/Solution.cs
@@ -0,0 +1,68 @@
+namespace LeetCodePractice.Console.LeetCodeTasks.SquaresOfSortedArray;
+
+public class Solution
+{
+
+    public static IEnumerable<TestCase<int[], int[]>> GetTestCases()
+    {
+        yield return new TestCase<int[], int[]>([-4, -1, 0, 3, 10], [0, 1, 9, 16, 100], new Solution().SortedSquares);
+        yield return new TestCase<int[], int[]>([-7, -3, 2, 3, 11], [4, 9, 9, 49, 121], new Solution().SortedSquares);
+    }
+
+    public int[] SortedSquares(int[] nums)
+    {
+        var sortedSquared = new int[nums.Length];
+        var firstPositiveNumberIndex = GetFirstPositiveNumberIndex(nums);
+
+        var forwardMovingIndex = firstPositiveNumberIndex;
+        var backwardsMovingIndex = firstPositiveNumberIndex - 1;
+        var sortedSquaredIndex = 0;
+
+        while (forwardMovingIndex < nums.Length || backwardsMovingIndex >= 0)
+        {
+            if (forwardMovingIndex >= nums.Length)
+            {
+                var value = nums[backwardsMovingIndex--];
+                sortedSquared[sortedSquaredIndex++] = value * value;
+                continue;
+            }
+
+            if (backwardsMovingIndex < 0)
+            {
+                var value = nums[forwardMovingIndex++];
+                sortedSquared[sortedSquaredIndex++] = value * value;
+                continue;
+            }
+
+            var forwardValue = nums[forwardMovingIndex];
+            var backwardValue = nums[backwardsMovingIndex];
+
+            var forwardValueSquared = forwardValue * forwardValue;
+            var backwardValueSquared = backwardValue * backwardValue;
+
+            if (forwardValueSquared > backwardValueSquared)
+            {
+                backwardsMovingIndex--;
+                sortedSquared[sortedSquaredIndex++] = backwardValueSquared;
+            }
+            else
+            {
+                forwardMovingIndex++;
+                sortedSquared[sortedSquaredIndex++] = forwardValueSquared;
+            }
+        }
+
+        return sortedSquared;
+    }
+
+    private static int GetFirstPositiveNumberIndex(int[] nums)
+    {
+        var index = Array.BinarySearch(nums, 0);
+
+        return index switch
+        {
+            < 0 => ~index,
+            _ => index
+        };
+    }
+}
--- LeetCodePractice.Console/Program.cs
+++ LeetCodePractice.Console/Program.cs
@@ -8,7 +8,7 @@ public static class Program
 {
     public static void Main()
     {
-        foreach (var testCase in LeetCodePractice.Console.LeetCodeTasks.FurthestBuildingYouCanReach.Solution.GetTestCases())
+        foreach (var testCase in LeetCodeTasks.SquaresOfSortedArray.Solution.GetTestCases())
         {
             try
             {
