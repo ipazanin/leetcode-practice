33c28a9c1f9e43cccf6c9e7a9ab39bfea121d8e1
ipazanin <ivan.pazanin1996@gmail.com>
2024-03-08 10:57:48 +0100

    Add CountElementsWithMaxFrequency C# Solution

---
+++ csharp/LeetCodePractice.Console/LeetCodeTasks/CountElementsWithMaxFrequency/Solution.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace LeetCodePractice.Console.LeetCodeTasks.CountElementsWithMaxFrequency;
+
+public class Solution
+{
+    public static IEnumerable<TestCase<int, int[]>> GetTestCases()
+    {
+        yield return new TestCase<int, int[]>(4, [1, 2, 2, 3, 1, 4], new Solution().MaxFrequencyElements);
+        yield return new TestCase<int, int[]>(5, [1, 2, 3, 4, 5], new Solution().MaxFrequencyElements);
+    }
+
+    public int MaxFrequencyElements(int[] nums)
+    {
+        return nums
+            .GroupBy(number => number)
+            .GroupBy(numberCountGrouping => numberCountGrouping.Count())
+            .OrderByDescending(numberWithCountGrouping => numberWithCountGrouping.Key)
+            .First()
+            .Sum(numberCountGrouping => numberCountGrouping.Count());
+    }
+}
